d8ff29406c59fb9880b56ef0581cb81ce510db20
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-22 21:54:07 +0530

    Create port_scanner.h

---
+++ backend/port_scanner.h
@@ -0,0 +1,9 @@
+#ifndef PORT_SCANNER_H
+#define PORT_SCANNER_H
+
+#include <string>
+
+// Define the function to scan ports
+std::string portScanner(const std::string& ip, int startPort, int endPort);
+
+#endif
